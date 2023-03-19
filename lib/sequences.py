a2537b5de1bbed5ca3181eff94c0e3282170cdc8
Myles Reyburn <mylesreyburn@gmail.com>
2023-03-18 18:35:35 -0600

    done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_seq = [0, 1]
+    if length == 0:
+        fib_seq.clear()
+    if length == 1:
+        fib_seq.pop()
+
+    i = 1
+
+    while i <= length:
+        
+        current_value = fib_seq[len(fib_seq) - 1]
+        penultimate_value = fib_seq[len(fib_seq) - 2]
+        next_value = current_value + penultimate_value
+        fib_seq.append(next_value)
+        i += 1
+
+    print(fib_seq[0:length])
+
+print_fibonacci(10)
